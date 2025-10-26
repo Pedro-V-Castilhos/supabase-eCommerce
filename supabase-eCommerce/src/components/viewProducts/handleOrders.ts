a725a0b860f6cb6a1e65185225dbdf7bda978da9
PedroVC <pedro.viana.castilhos@gmail.com>
2025-10-26 13:54:44 -0300

    :sparkles: feat: Função de adicionar a quantidade caso o produto já esteja no pedido

--- supabase-eCommerce/src/components/viewProducts/handleOrders.ts
+++ supabase-eCommerce/src/components/viewProducts/handleOrders.ts
@@ -1,5 +1,6 @@
 import { supabase } from "../../supabase-client"
 import type Order from "../../types/order"
+import type OrderHasProduct from "../../types/orderHasProduct"
 
 export const newOrder = async () => {
     const idUser:string | undefined = (await supabase.auth.getSession()).data.session?.user.id
@@ -30,11 +31,31 @@ export const fetchOrders = async() => {
 
 export const insertProductToOrder = async (id:number) => {
     const currentOrder:Order[] = await fetchOrders() || []
+    const orderHasProuct:OrderHasProduct[] = await checkIfProductInOrder(id, currentOrder[0].id) || []
 
-    const {error} = await supabase.from("order_has_products").insert({orderId: currentOrder[0].id,productId: id, quantity: 1})
+    console.log(...orderHasProuct)
+    if(orderHasProuct){
+        const {error} = await supabase.from("order_has_products").update({quantity: orderHasProuct[0].quantity + 1}).eq("id", orderHasProuct[0].id)
+        if(error){
+            console.log(error.message)
+            return
+        }
+    }else{
+        const {error} = await supabase.from("order_has_products").insert({orderId: currentOrder[0].id,productId: id, quantity: 1})
+        if(error){
+            console.log(error.message)
+            return
+        }
+    }
+}
 
+export const checkIfProductInOrder = async (productId:number, orderId:number) => {
+    const {error, data} = await supabase.from("order_has_products").select("*").eq("productId", productId).eq("orderId", orderId)
+    
     if(error){
         console.log(error.message)
         return
     }
+
+    return data
 }
---
+++ supabase-eCommerce/src/types/orderHasProduct.ts
@@ -0,0 +1,6 @@
+export default interface OrderHasProduct{
+    id: number,
+    orderId: number,
+    productId: number,
+    quantity: number,
+}
